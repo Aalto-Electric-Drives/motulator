9ec1618e134927d3c1667a28953bef3363988153
Marko Hinkkanen <76600872+mhinkkan@users.noreply.github.com>
2023-01-15 19:44:08 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,7 +19,7 @@ sys.path.insert(0, os.path.abspath('../../motulator'))
 # -- Project information -----------------------------------------------------
 
 project = 'motulator'
-copyright = '2022, Aalto Electric Drives'
+copyright = '2023, Aalto Electric Drives'
 author = 'Aalto Electric Drives'
 
 # The full version, including alpha/beta/rc tags
