5e1546dcf84d31928be947bba8be84f3a4068157
Marko Hinkkanen <76600872+mhinkkan@users.noreply.github.com>
2023-04-18 09:15:36 +0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -84,9 +84,11 @@ exclude_patterns = ['../../.idea', '../../__pycache__', '../../venv']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-# html_theme = 'alabaster'
-# html_theme = 'sphinx_rtd_theme'
 html_theme = 'sphinx_book_theme'
+html_theme_options = {
+    "repository_url": "https://github.com/Aalto-Electric-Drives/motulator",
+    "use_repository_button": True,
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
