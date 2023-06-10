9d51992ed9f3f3e2f9c6a3a4386607bf7cd61d4e
Marko Hinkkanen <76600872+mhinkkan@users.noreply.github.com>
2023-06-10 14:45:10 +0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,7 +25,7 @@ copyright = '2023, Aalto Electric Drives'
 author = 'Aalto Electric Drives'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.5'
+release = '0.1.6'
 
 # -- General configuration ---------------------------------------------------
 
