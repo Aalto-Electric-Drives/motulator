301d1884f8a60b16910ff639af479e0d0d276c5f
Marko Hinkkanen <marko.hinkkanen@aalto.fi>
2024-05-28 16:19:21 +0300

    Testing Sphinx

--- motulator/model/__init__.py
+++ motulator/model/__init__.py
@@ -4,7 +4,7 @@ from motulator.model._mechanics import Mechanics, MechanicsTwoMass
 from motulator.model._converter import FrequencyConverter, Inverter
 from motulator.model._lc_filter import LCFilter
 from motulator.model._simulation import (
-    CarrierComparison, Model, Simulation, Delay, zoh)
+    CarrierComparison, Delay, Model, Simulation, zoh)
 
 from motulator.model import im, sm
 
